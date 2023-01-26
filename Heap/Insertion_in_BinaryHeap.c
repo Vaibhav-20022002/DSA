2292783ef5f1b520d31846be5d1094a5cce1cefa
Vaibhav-20022002 <89464371+Vaibhav-20022002@users.noreply.github.com>
2023-01-26 12:37:50 +0530

    Create Insertion_in_BinaryHeap.c

---
+++ Heap/Insertion_in_BinaryHeap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void insertbheap(int arr[],int n){
+
+}
+int main(void)
+{
+    int arr[10] = {1,3,6,5,9,8};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    printf("ORIGINAL BINARY HEAP: ");
+    insertbheap(arr,size);
+    return 0;
+}
+//CANNOT BE DONE BECAUSE I DON'T KNOW HOW TO MANUPULATE THE SIZE OF ARRAY AT THE TIME OF EXECUTION !!!!
