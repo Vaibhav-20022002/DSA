c1e1e1b6d7d22f735676fbf0ee1e98f0f42266ec
Vaibhav-20022002 <89464371+Vaibhav-20022002@users.noreply.github.com>
2023-02-09 20:56:51 +0530

    Create Priority_Queue_using_Heap.c

---
+++ Queue/Priority_Queue_using_Heap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void insert(int value,int arr[],int size){
+    if(size > -1){
+        
+    }
+    else if(size==-1){
+        arr[0]=value;
+    }
+}
+int main(void)
+{
+    int arr[10];
+    int size=-1;
+    insert(45,arr,size);
+    return 0;
+}
